9de37309388b2a6a29f12c494d1501c13b097c82
Matt Turner <matturner@gmail.com>
2020-12-26 15:29:37 +0000

    CICD improvements

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 declare base_url="${1:?need a base URL}"
 
 hugo --enableGitInfo -b https://${base_url} -s . -d public
--- build/preview.sh
+++ build/preview.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # This is used by forrestry, so if they change their requirements; eg they need it to bind to a different port, this will need updating>
 # Forrestry needs the --renderToDisk option
 hugo server -D -E -F --enableGitInfo --renderToDisk -d public
